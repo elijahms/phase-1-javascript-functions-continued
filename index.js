c1a0584495ef7c14b8f66eba9855704e8b4148a0
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-12 17:29:08 -0400

    commit tree 26b91e7f67a04c2ca9cbc943476f21d184fe49c8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const mondayWork = function(activity = "go to the office") {
+    if (activity != "go to the office") {
+        return `This Monday, I will ${activity}.`
+    }
+    else {
+        return `This Monday, I will ${activity}.`
+    }    
+}
+
+function wrapAdjective(thang = "*") {
+    return function(thing = "special") {
+        return `You are ${thang}${thing}${thang}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
+ 
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
